a88fd244144c8eb4821b7ccfdd2930a53e29c946
karuiel <miguelmoralescastillo95@gmail.com>
2015-04-03 16:51:55 +0200

    diagrama

---
+++ NapakalakiRB/pkg/NapakalakiRB-0.0.1/lib/treasure_kind.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+#
+#=treasure_kind.rb
+#
+#Author:: Miguel Morales Castillo y María del Mar Ruiz Martín
+#
+#==Módulo TreasureKind
+#En este módulo se recogen todos los tipos de tesoros posibles:
+#- ARMOR
+#- ONEHAND
+#- BOTHHANDS
+#- HELMET
+#- SHOE
+#- NECKLACE
+#
+
+module TreasureKind
+    ARMOR = :ARMOR
+    ONEHAND = :ONEHAND
+    BOTHHANDS = :BOTHHANDS
+    HELMET = :HELMET
+    SHOE = :SHOE
+    NECKLACE = :NECKLACE    
+end
+
