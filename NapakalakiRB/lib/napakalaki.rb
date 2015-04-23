595fd25ffecf6b502ebc6ac1b78d25b5e3d80abf
Mariadel <mariadel52@correo.ugr.es>
2015-04-23 13:23:25 +0200

    mariadel mola <3

--- NapakalakiRB/lib/napakalaki.rb
+++ NapakalakiRB/lib/napakalaki.rb
@@ -15,6 +15,7 @@ module Model
 
 
     def initialize
+      @currentPlayerIndex = -1;
       #@currentPlayer
       #@currentMonster
       #@players = Array.new
@@ -29,24 +30,33 @@ module Model
       
       end
 
-      #Nota: mimso problema que en java
       def nextPlayer
-        @currentPlayerIndex = (@currentPlayerIndex + 1) % @players.size 
+        
+        if(@currentPlayerIndex == -1)
+          @currentPlayerIndex = rand(@players.size)
+
+        else
+          @currentPlayerIndex = (@currentPlayerIndex + 1) % @players.size 
+        end 
+        
         @currentPlayer = @players.at(@currentPlayerIndex)
         @currentPlayer
 
       end
 
       def combat
-
+        result = @currentPlayer.combat(@currentMonster);
+        dealer = CardDealer.instance;
+        dealer.giveMonsterBack(@currentMonster);
+        return result;
       end
 
       def discardVisibleTreasure(t)
-
+        @currentPlayer.discardVisibleTreasure(t)
       end
 
       def discardHiddenTreasure(t)
-
+        @currentPlayer.discardVisibleTreasure(t) 
       end
 
       def makeTreasureVisible(t)
@@ -54,20 +64,46 @@ module Model
       end
 
       def buyLevels(visible, hidden)
-
+        return @currentPlayer.buyLevels(visible, hidden)
       end
 
       def initGame(players)
-
+        dealer = CardDealer.instance
+        dealer.initCards()
+        initPlayers(players)
+        nextTurn();
+      end
+      
+      def getCurrentPlayer
+        @currentPlayer
+      end
+      
+      def getCurrentMonster
+        @currentMonster
       end
 
       def canMakeTreasureVisible(t)
-
+        canMake = @currentPlayer.canMakeTreasureVisible(t);
+        
+        if(canMake)
+            @currentPlayer.makeTreasureVisible(t);
+        end
+         canMake;
       end
 
       def  nextTurn
-
+        allowed = nextTurnAllowed();
+        if(allowed)
+            dealer = CardDealer.instance
+            @currentMonster = dealer.nextMonster
+            @currentPlayer = nextPlayer()
+            if(@currentPlayer.isDead)
+                @currentPlayer.initTreasures() 
+            end
+        end    
+        allowed;
       end
+      
       def  nextTurnAllowed
           @currentPlayer.validState
       end
